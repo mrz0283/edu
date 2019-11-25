b2f1c7de2b9169236879db51dbd507cc5a2cc9ba
麻睿哲 <lijh@aixahc.com>
2019-11-25 15:06:26 +0800

    'edu开始'

---
+++ edu_son_project/src/main/java/com/online/edu/eduservice/controller/EduTeacherController.java
@@ -0,0 +1,21 @@
+package com.online.edu.eduservice.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 讲师 前端控制器
+ * </p>
+ *
+ * @author mrz
+ * @since 2019-11-24
+ */
+@RestController
+@RequestMapping("/eduservice/edu-teacher")
+public class EduTeacherController {
+
+}
+
---
+++ edu_son_project/src/main/java/com/online/edu/eduservice/service/EduTeacherService.java
@@ -0,0 +1,16 @@
+package com.online.edu.eduservice.service;
+
+import com.online.edu.eduservice.entity.EduTeacher;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 讲师 服务类
+ * </p>
+ *
+ * @author mrz
+ * @since 2019-11-24
+ */
+public interface EduTeacherService extends IService<EduTeacher> {
+
+}
